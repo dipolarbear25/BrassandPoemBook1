ae7e1a151f017aefc6ea7292f82c54b67d907fa1
Austin Mangum <mangumaustin@gmail.com>
2024-01-20 10:58:13 -0600

    Completed!

--- BrassAndPoem/Program.cs
+++ BrassAndPoem/Program.cs
@@ -112,17 +112,139 @@ string productsDetails(Product products)
 
 void DeleteProduct(List<Product> products, List<ProductType> productTypes)
 {
-    throw new NotImplementedException();
+    Console.WriteLine("Please select an item to remove form the inventory.");
+
+    for (int i = 0; i < products.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {products[i].Name}");
+    }
+
+    Product chosenProduct = null;
+    string userResponse = "";
+
+    while (chosenProduct == null)
+    {
+        Console.WriteLine("please enter the product number here: ");
+        try
+        {
+            int answer = int.Parse(Console.ReadLine().Trim());
+
+            chosenProduct = products[answer - 1];
+
+            Console.WriteLine(@$"Are you sure you want to remove {chosenProduct.Name}?
+                1. Yes, remove it.
+                2. cancel");
+
+            try
+            {
+                userResponse = Console.ReadLine();
+                if (userResponse == "1")
+                {
+                    products.RemoveAt(answer - 1);
+                    Console.WriteLine($"You have removed {chosenProduct.Name}");
+                    DisplayMenu();
+                }
+                else if (userResponse == "2")
+                {
+                    DisplayMenu();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Please select 1 to remove and 2 to cancel.");
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Numbers are only accepted, please try again.");
+        }
+    }
 }
 
 void AddProduct(List<Product> products, List<ProductType> productTypes)
 {
-    throw new NotImplementedException();
+    Console.WriteLine("Please enter the products name: ");
+
+    string responseName = Console.ReadLine();
+
+    Console.WriteLine($@"Please select which type of product {responseName} is:
+    0. Brass
+    1. Poem");
+
+    int responseType = int.Parse(Console.ReadLine());
+
+    Console.WriteLine($"Please enter the price of {responseName}: ");
+
+    int responsePrice = int.Parse(Console.ReadLine());
+
+    Product newProduct = new Product
+    {
+        Name = responseName,
+        ProductTypeId = responseType,
+        Price = responsePrice,
+    };
+
+    products.Add(newProduct);
+
+    Console.WriteLine($"You've added {newProduct.Name}! it is priced at ${newProduct.Price}!");
 }
 
 void UpdateProduct(List<Product> products, List<ProductType> productTypes)
 {
-    throw new NotImplementedException();
+    Console.WriteLine("Please choose a product to update:");
+
+    for (int i = 0; i < products.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {products[i].Name}");
+    }
+
+    int userChoice = int.Parse(Console.ReadLine().Trim());
+    int productToUpdate = userChoice - 1;
+
+    Product chosenProduct = products[productToUpdate];
+    Console.WriteLine($"You chose {chosenProduct.Name}. Its type is {chosenProduct.ProductTypeId} and its price is {chosenProduct.Price}");
+    Console.WriteLine("Insert New Name");
+    string response1 = Console.ReadLine();
+    Console.WriteLine("Insert New Type");
+    string response2 = Console.ReadLine();
+    Console.WriteLine("Insert New Price");
+    string response3 = Console.ReadLine();
+
+    if (!string.IsNullOrEmpty(response1))
+    {
+        chosenProduct.Name = response1;
+    }
+    else
+    {
+        chosenProduct.Name = chosenProduct.Name;
+    }
+    if (!string.IsNullOrEmpty(response2))
+    {
+        int productId = int.Parse(response2);
+        chosenProduct.ProductTypeId = productId;
+    }
+    else
+    {
+        chosenProduct.ProductTypeId = chosenProduct.ProductTypeId;
+    }
+    if (!string.IsNullOrEmpty(response3))
+    {
+        chosenProduct.Price = int.Parse(response3);
+    }
+    else
+    {
+        chosenProduct.Price = chosenProduct.Price;
+    }
+
+    Product updatedProduct = new Product
+    {
+        Name = chosenProduct.Name,
+        ProductTypeId = chosenProduct.ProductTypeId,
+        Price = chosenProduct.Price
+    };
+
+    Console.WriteLine("The product was updated.");
+
 }
 Greeting();
 DisplayMenu();
